38da8db6d84f72dc9f33fdf0fecd2fe543414e8b
Latias94 <superfrankie621@gmail.com>
2022-07-05 18:26:14 +0800

    ch01

---
+++ os/src/console.rs
@@ -0,0 +1,40 @@
+/*！
+
+本模块实现了 print 和 println 宏。
+
+*/
+use core::fmt::{self, Write};
+
+use crate::syscall;
+
+const SYSCALL_WRITE: usize = 64;
+
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        sys_write(1, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg:tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(,($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
